3dcff2c1d8065228550e3785b848c01dc582e087
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-29 19:40:39 +0530

    rendered the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-
-import './App.css'
-import WolfPet from './Components/WolfPet'
+import React from 'react';
+import Navbar from './components/Header/Navbar';
+import Hero from './components/Hero/Hero';
+import ProjectsSection from './components/Projects/ProjectsSection';
+import ExperienceSection from './components/Experience/ExperienceSection';
+import TechStackSection from './components/TechStack/TechStackSection';
+import EducationSection from './components/Education/EducationSection';
+import ContactFooter from './components/Footer/ContactFooter';
+import useScrollReveal from './hooks/useScrollReveal';
+import useTesseractCanvas from './hooks/useTesseractCanvas';
+import useInkTrailCanvas from './hooks/useInkTrailCanvas';
+import './index.css';
 
 function App() {
+  useScrollReveal();
+  useTesseractCanvas();
+  useInkTrailCanvas();
 
   return (
     <>
-      <WolfPet/>
+      <Navbar />
+      <Hero />
+      <ProjectsSection />
+      <ExperienceSection />
+      <TechStackSection />
+      <EducationSection />
+      <ContactFooter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
